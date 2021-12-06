7dabb1ae7314753da008c243f957ac804b76d62c
Ricardo-MayaHdz <95257266+Ricardo-MayaHdz@users.noreply.github.com>
2021-12-06 15:38:08 -0600

    Add files via upload

---
+++ conexion2.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+    $username = "root";
+  	$password = "";
+  	$dbname = "ligasdefutbol";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+      if($conn->connect_error){
+        die("Conexión fallida: ".$conn->connect_error);
+      }
+?>
